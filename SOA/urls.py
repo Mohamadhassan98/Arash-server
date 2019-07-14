11aa3f8d05d25daafd292b983948f81749df1e9f
aghabozorgi <faezeeaghabozorgi@gmail.com>
2019-07-14 15:56:05 +0430

    login and add Arash and license

--- SOA/urls.py
+++ SOA/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import Signup, GetUser
+from .views import *
 
 urlpatterns = [
     path('signup/', Signup.as_view()),
-    path('user/<int:id>/', GetUser.as_view())
+    path('login/', Login.as_view()),
+    path('user/<int:id>/', GetUser.as_view()),
+    path('add/arash/', AddArash.as_view()),
+    path('add/license/', AddLicense.as_view()),
+
 
 ]
--- SOA/views.py
+++ SOA/views.py
@@ -1,14 +1,14 @@
 from django.contrib.auth import login
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from support.models import User
-from support.serializers import UserSerializer
+from .serializers import *
 
 
 class Signup(APIView):
-    permission_classes = []
+    permission_classes = (AllowAny,)
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -17,6 +17,23 @@ class Signup(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Login(APIView):
+    authentication_classes = []
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        print('start')
+        username = request.data['username']
+        password = request.data['password']
+        user = User.objects.filter(username=username)
+        print(user[0])
+        if user.count() == 1:
+            if user[0].check_password(password):
+                login(request, user[0])
+                return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 class GetUser(APIView):
@@ -30,3 +47,23 @@ class GetUser(APIView):
             return Response(serializer.data)
         except User.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class AddArash(APIView):
+    def post(self, request):
+        serializer = ArashSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AddLicense(APIView):
+    def post(self, request):
+        serializer = LicenseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
